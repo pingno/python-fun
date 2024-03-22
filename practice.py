aeeda7913dc4e77bbaf92f2fc4cb8645a1127388
peang <pngo93@gmail.com>
2024-03-22 13:41:18 -0500

    python practice

---
+++ practice.py
@@ -0,0 +1,54 @@
+print("Hello, World!")
+
+
+list1 = ["Apple", "Cherry", "Orange"]
+
+tuple1 = ("This", "is", "a", "tuple") #immutable elements cannot be changed, added, or removed
+
+dict1 = {"name": "john", "age": 36}  #object
+
+set1 = {"luigi", "mario", "peach"}
+
+
+#list methods (like advanced array methods)
+'''
+append() -> adds to the end of a list
+extend() -> joins two lists together
+insert() -> (el, index) can sert insert an element at specified index
+remove() -> removes the first occurence of a specified element
+pop() -> remove and reutrn the element (can specify index or empty removes last index)
+clear() -> removes all elements from the list
+
+'''
+
+def forloop():
+    for x in list1:
+        if x == "Apple":
+            print("true")
+        else:
+            print("false")
+
+# forloop()
+
+def range1():
+    for i in range(10):
+        print(i)
+
+# range1() 
+# output is 0 -> 9  (not inclusive)
+
+def rangelen1():
+    for x in range(len(list1)):
+        print(x)
+
+rangelen1()
+#output 0, 1, 2 
+
+
+#lambda (ARROW FUNCTION)
+
+x = lambda a : a + 10
+print(x(5))
+
+#func_name = lambda arg : func
+#output 15
