0f086784ab6ce192c7144e6d9c6e28886d4e2118
peang <pngo93@gmail.com>
2024-04-08 22:56:32 -0500

    dict pra

--- dict.py
+++ dict.py
@@ -1,1 +1,17 @@
 # Dictionaries ( like objects in JS ) 
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(thisdict)
+
+print(thisdict["brand"])
+
+print("the length of this dictionary is " + len(thisdict))
+
+#Dictionary Items - Data Types
+# The values in dictionary items can be of any data type:
+
