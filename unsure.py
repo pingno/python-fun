f968e6971bb91eae3bbe55a3ee23343ca2f2e88c
peang <pngo93@gmail.com>
2024-04-12 22:34:39 -0500

    list methods

--- unsure.py
+++ unsure.py
@@ -69,3 +69,20 @@ print(thislist)
 copythislist = thislist.copy()
 copynumlist = list(numlist)
 print(copynumlist)
+
+
+'''
+
+append() - adds an element at the end of the list
+clear() - removes all the elments from the list
+copy() - return sa copy of the list
+count() - returns the number of elements with the specified value
+extend() - add the elements of a list (or any iterable), to the end of the current list
+index() - returns the index of the first element with the specified value
+insert() - adds an element at the specified position
+pop() - removes the element at the specified position
+remove() - removes the item with the specified value
+reverse() - reverse the order of the list
+sort() - sorts the list
+
+'''
