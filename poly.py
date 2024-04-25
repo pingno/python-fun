14e6bd710125b6ebc650199cba45e51611a2853e
peang <pngo93@gmail.com>
2024-04-25 14:56:13 -0500

    inheritance class polymorphism

--- poly.py
+++ poly.py
@@ -1,30 +1,86 @@
-class Car:
+'''
+Function Polymorphism
+
+An example of a Python function that can be used on different objects is the len() function
+
+String
+for string lens() returns the number of chars
+
+Tuple
+for tuples len() reutrns the number of items in the tuple
+
+Dictionaries
+for dictionaries len() returns the number of key/value pairs in the dictionary:
+'''
+
+
+
+# class Car:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Drive!")
+
+# class Boat:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Sail!")
+
+# class Plane:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Fly!")
+
+# car1 = Car("Ford", "Mustang")       #Create a Car class
+# boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+# plane1 = Plane("Boeing", "747")     #Create a Plane class
+
+# for x in (car1, boat1, plane1):
+#   x.move()
+
+
+'''
+Inheritance Class Polymorphism
+What about classes with child classes with the same name? Can we use polymorphism there?
+
+Yes. If we use the example above and make a parent class called Vehicle, 
+and make Car, Boat, Plane child classes of Vehicle, 
+the child classes inherits the Vehicle methods, but can override them:
+'''
+
+
+class Vehicle:
   def __init__(self, brand, model):
     self.brand = brand
     self.model = model
 
   def move(self):
-    print("Drive!")
+    print("Move!")
 
-class Boat:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
+class Car(Vehicle):
+  pass
 
+class Boat(Vehicle):
   def move(self):
     print("Sail!")
 
-class Plane:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
-
+class Plane(Vehicle):
   def move(self):
     print("Fly!")
 
-car1 = Car("Ford", "Mustang")       #Create a Car class
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
-plane1 = Plane("Boeing", "747")     #Create a Plane class
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
 
 for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
   x.move()
